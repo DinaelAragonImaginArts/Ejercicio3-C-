d4641828db3774048ef6cb0afc351f0d7c724780
Dinael Aragon <84431313+DinaelAragonImaginArts@users.noreply.github.com>
2023-04-24 10:18:31 -0700

    Add files via upload

---
+++ Ejercicio tema 3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int main(int a, int b, int c)
+        {
+            int resultado = a + b + c;
+            Console.WriteLine(resultado);
+            return resultado;
+        }
+        main(3, 4, 4);
+    }
+    //Segunda parte
+    public class Coche
+    {
+        public int numeroPuertas { get; set;}
+        public Coche(int puertas){
+            numeroPuertas =  puertas;
+        }
+        public void changePuertas(int puertas){
+            numeroPuertas = numeroPuertas + puertas;
+        }
+    }
+}
